ef42b8533586523419590b7efd2f396177549b11
nicatcod <qaqa0qaqa002@gmail.com>
2024-07-29 18:38:02 +0400

    YeniOyrendiklerim11

--- src/Collections_/MAPinterface.java
+++ src/Collections_/MAPinterface.java
@@ -10,6 +10,7 @@ public class MAPinterface {
         HashMap map = new HashMap();
         map.put("A", 1);
         map.put("B", 2);
+
         map.put("C", 3);
         map.put("D", 4);
         map.put("E", 5);
